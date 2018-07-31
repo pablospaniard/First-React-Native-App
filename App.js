4b678bdf6100e0df15d513691e542a0960e4cd06
Pavel Ivanov <pablospaniard@gmail.com>
2018-07-31 09:33:37 +0200

    before first assignment

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {StyleSheet, Text, View, TextInput, Button} from 'react-native'
 
+import ListItem from './src/components/ListItem/ListItem'
+
 export default class App extends React.Component {
   state = {
     placeName: '',
@@ -26,7 +28,7 @@ export default class App extends React.Component {
 
   render() {
     const placesOutput = this.state.places.map((place, i) => (
-      <Text key={i}>{place}</Text>
+      <ListItem key={i} placeName={place} />
     ))
     return (
       <View style={styles.container}>
@@ -43,7 +45,7 @@ export default class App extends React.Component {
             onPress={this.placeSubmitHandler}
           />
         </View>
-        <View>{placesOutput}</View>
+        <View style={styles.listContainer}>{placesOutput}</View>
       </View>
     )
   }
@@ -68,5 +70,8 @@ const styles = StyleSheet.create({
   },
   inputButton: {
     width: '30%'
+  },
+  listContainer: {
+    width: '100%'
   }
 })
