ca0f4c7ecfa99e33aa6df9ac5c69cce3fed44ecd
Pavel Ivanov <pablospaniard@gmail.com>
2018-08-08 15:53:27 +0200

    all works

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
-import {View, Text, Button} from 'react-native'
+import {View, Text, StyleSheet, ImageBackground} from 'react-native'
 
 import startTabs from '../MainTabs/StartMainTabs'
+import {
+  DefaultInput,
+  HeaderText,
+  MainText,
+  ButtonWithBackground
+} from '../../components/UI'
+import backgroundImage from '../../assets/background.jpg'
 
 class Auth extends Component {
   loginHandler = () => {
@@ -9,12 +16,63 @@ class Auth extends Component {
   }
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
-      </View>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeaderText>Please Log in</HeaderText>
+          </MainText>
+          <View style={styles.inputContainer}>
+            <DefaultInput
+              placeholder="Your Email Address"
+              style={styles.input}
+            />
+            <DefaultInput placeholder="Password" style={styles.input} />
+            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+          </View>
+          <View style={styles.buttonsContainer}>
+            <ButtonWithBackground
+              color="#f47329"
+              onPress={() => {
+                alert('Hello')
+              }}
+            >
+              Switch to Login
+            </ButtonWithBackground>
+            <MainText>
+              <Text>or</Text>
+            </MainText>
+            <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+              Submit
+            </ButtonWithBackground>
+          </View>
+        </View>
+      </ImageBackground>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: '#eee',
+    borderColor: '#bbb'
+  },
+  backgroundImage: {
+    width: '100%',
+    flex: 1
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+})
 
 export default Auth
